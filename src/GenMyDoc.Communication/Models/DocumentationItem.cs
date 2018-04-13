5c50f3dcc05fda6e4da4cb9aaf259d8d7497b5c1
cdie <christophe.mommer@gmail.com>
2018-04-13 10:18:47 +0200

    intermediate commit

---
+++ src/GenMyDoc.Communication/Models/DocumentationItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenMyDoc.Communication.Models
+{
+    public sealed class DocumentationItem
+    {
+
+        #region Properties
+
+        public Guid Id { get; internal set; }
+        public Guid? ParentId { get; internal set; }
+        public string Name { get; internal set; }
+
+        #endregion
+
+    }
+}
